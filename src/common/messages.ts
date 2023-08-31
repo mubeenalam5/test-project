31a160f49a46f41610c934d2cdadcf4d94db23a5
mubeen <mubeenalam5@gmail.com>
2023-08-31 17:33:20 +0500

    change

--- src/common/messages.ts
+++ src/common/messages.ts
@@ -2,5 +2,6 @@ export const messages = {
     sign_up: 'Resgistered Successfully',
     sign_in: 'Signed In Successfully',
     already_exist: 'Already Exist',
+    invalid_credentials: 'Invalid Credentials'
   };
   
