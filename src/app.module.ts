8eecf7670be305f952c1d15232978566612eeb4a
mubeen <mubeenalam5@gmail.com>
2023-09-01 19:19:17 +0500

    changes

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,4 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule } from '@nestjs/config';
@@ -21,9 +19,7 @@ import { PostModule } from './post/post.module';
     AuthModule,
     PostModule
   ],
-  controllers: [AppController],
   providers: [
-    AppService,
     AuthService,
     RolesGuard,
     JwtAuthGuard,
--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -14,6 +14,7 @@ import { Role } from './role.enum';
 export class AuthController {
   constructor(private readonly authService: AuthService) { }
 
+  @ApiTags(Role.Admin)
   @UseInterceptors(TransformInterceptor)
   @Post('admin-signup')
   adminSignUp(@Body() adminSignUpDto: AdminSignUpDto) {
